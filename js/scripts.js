17f1a755d69bb4edb20fc8b9f65908a63078b575
marko <marko.barraza@gmail.com>
2020-06-29 21:08:47 -0400

    plantilla consolas

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+'use strict'
+
+// swiper consola
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 5,
+  freeMode: true,
+  // init: false,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  }
+});
